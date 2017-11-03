230f83101d887d7d06be9cccefb810e45179d800
edwinsu <1076083782@qq.com>
2017-11-03 17:12:03 +0800

    修改了创建表格工具，加入秘钥验证

--- PDIWT_Gadget/CreateTableTool.cpp
+++ PDIWT_Gadget/CreateTableTool.cpp
@@ -1,19 +1,35 @@
 #include "PDIWT_Gadget.h"
 
+using namespace System::Windows::Forms;
+using namespace OfficeOpenXml;
+using namespace msclr::interop;
+
 extern TableYTolerance g_tableytolerance;
 struct TextInfo
 {
 	WString m_textcontent;
 	DPoint3d origin;
-}; 
+};
+
+struct TabelCellInfo
+{
+	size_t row;
+	size_t column;
+	DRange2d cellrange;
+	WString text;
+};
 
 struct CreateTableTool : DgnElementSetTool
 {
-private :
-	std::list<TextInfo> tabelInfo;
+private:
+	//Extract the textInfo
+	void _GetGridXYRange(bstdset<double>& _xrange, bstdset<double>& _yrange);
+	void _GetGridCellInfo(bvector<TabelCellInfo>& _tablecellsinfo, const bstdset<double> _xrange, const bstdset<double> _yrange);
+	void _ExtractTextInfo(bvector<TextInfo>& _selectedtextesinfo);
+
 
 protected:
-	CreateTableTool(int toolId) : DgnElementSetTool(toolId){}
+	CreateTableTool(int toolId) : DgnElementSetTool(toolId) {}
 	virtual StatusInt _OnElementModify(EditElementHandleR el) override;
 	//state logic
 	virtual bool _NeedAcceptPoint() override { return true; }
@@ -26,17 +42,119 @@ protected:
 	virtual void _OnRestartTool() override { InstallNewInstance(GetToolId()); }
 	virtual bool _OnResetButton(DgnButtonEventCR ev) override { _OnRestartTool(); return true; }
 	//Populating Elements
-	virtual bool _FilterAgendaEntries() override;
+	//virtual bool _FilterAgendaEntries() override;
 	virtual bool _OnModifyComplete(DgnButtonEventCR ev) override;
 
-	//Extract the textInfo
-	StatusInt _ExtractTextInfo();
-	void OutputTable(BeFileName outFileName);
-	
+
 public:
 	static void InstallNewInstance(int toolId);
-	
+	void _OutputTable(WString outFileName);
 };
+
+void CreateTableTool::_OutputTable(WString outFileName)
+{
+	bstdset<double> _xrange, _yrange;
+	_GetGridXYRange(_xrange, _yrange);
+	if (_xrange.size() > 0 && _yrange.size() > 0)
+	{
+		bvector<TabelCellInfo> _tabelcellinfo;
+		_GetGridCellInfo(_tabelcellinfo, _xrange, _yrange);
+		bvector<TextInfo> _selectedtextesinfo;
+		_ExtractTextInfo(_selectedtextesinfo);
+
+		for (auto _textinfo : _selectedtextesinfo)
+		{
+			auto itr = std::find_if(_tabelcellinfo.begin(), _tabelcellinfo.end(), [&_textinfo](TabelCellInfo _tt) { return _tt.cellrange.Contains(_textinfo.origin); });
+			if (itr != _tabelcellinfo.end()) itr->text += _textinfo.m_textcontent;
+		}
+
+		System::String^ _filePath = marshal_as<System::String^>(outFileName.c_str());
+		ExcelPackage^ _package = gcnew ExcelPackage(gcnew System::IO::FileInfo(_filePath));
+		ExcelWorksheet^ _sheet = _package->Workbook->Worksheets->Add(System::IO::Path::GetFileNameWithoutExtension(_filePath));
+		for each(auto _cellinfo in _tabelcellinfo)
+		{
+			mdlDialog_dmsgsPrint(WPrintfString(L"row:{%d}, col:{%d}, Text {%s}", _cellinfo.row, _cellinfo.column, _cellinfo.text));
+
+			//_sheet->Cells[_cellinfo.row, _cellinfo.column]->Value = marshal_as<System::String^>(_cellinfo.text);
+		}
+		_package->Save();
+	}
+}
+
+//  [11/3/2017 sudongsheng]
+//	获得表格线的从小到大的_xrange和_yrange
+void CreateTableTool::_GetGridXYRange(bstdset<double>& _xrange, bstdset<double>& _yrange)
+{
+	ElementAgendaR _eleagenda = GetElementAgenda();
+	for (auto _ele : _eleagenda)
+	{
+		if (_ele.GetElementType() == LINE_ELM)
+		{
+			LineHandler* _line_handlerP = dynamic_cast<LineHandler*>(_ele.GetDisplayHandler());
+			if (nullptr == _line_handlerP) continue;
+			CurveVectorPtr _linecv;
+			_line_handlerP->GetCurveVector(_ele, _linecv);
+			DVec3d _lineVector;
+			DPoint3d _lineStartPoint;
+			_linecv->GetStartEnd(_lineStartPoint, DPoint3d(), _lineVector, DVec3d());
+			if (_lineVector.IsParallelTo(DVec3d::UnitX()))
+			{
+				_yrange.insert(_lineStartPoint.y);
+			}
+			else if (_lineVector.IsParallelTo(DVec3d::UnitY()))
+			{
+				_xrange.insert(_lineStartPoint.x);
+			}
+		}
+	}
+}
+
+// 将单元格行列数与单元格【起始索引为1,1】在二维空间中的范围联系起来
+void CreateTableTool::_GetGridCellInfo(bvector<TabelCellInfo>& _tablecellsinfo, const bstdset<double> _xrange, const bstdset<double> _yrange)
+{
+	bvector<double> _xrangevector(_xrange.begin(), _xrange.end());
+	bvector<double> _yrangevector(_yrange.rbegin(), _yrange.rend());
+
+	size_t _totalrownum = _xrange.size() - 1;
+	size_t _totalcolnum = _yrange.size() - 1;
+
+	for (size_t _row = 0; _row < _totalrownum; _row++)
+	{
+		for (size_t _col = 0; _col < _totalcolnum; _col++)
+		{
+			TabelCellInfo _cell = { _row + 1,_col + 1,DRange2d::From(_xrangevector[_row],_yrangevector[_col + 1],_xrangevector[_row + 1],_yrangevector[_col]),L"" };
+			_tablecellsinfo.push_back(_cell);
+		}
+	}
+}
+
+//获得所选择的所有文字信息和位置信息
+void CreateTableTool::_ExtractTextInfo(bvector<TextInfo>& _selectedtextesinfo)
+{
+	_selectedtextesinfo.clear();
+	ElementAgendaR agd = GetElementAgenda();
+	for (auto ele : agd)
+	{
+		if (ele.GetElementType() != TEXT_ELM && ele.GetElementType() != TEXT_NODE_ELM)
+			continue;
+
+		ITextQueryCP textQueryHandler = ele.GetITextQuery();
+		if (textQueryHandler == nullptr) continue;
+		T_ITextPartIdPtrVector textPartIds;
+		textQueryHandler->GetTextPartIds(ele, *ITextQueryOptions::CreateDefault(), textPartIds);
+		//Create TextInfo
+		TextInfo textinfos;
+		for each (auto textPartId in textPartIds)
+		{
+			TextBlockPtr textBlock = textQueryHandler->GetTextPart(ele, *textPartId);
+			textinfos.m_textcontent += textBlock->ToString();
+		}
+		DisplayHandlerP  textDisplayHandler = ele.GetDisplayHandler();
+		if (textDisplayHandler == nullptr) continue;
+		textDisplayHandler->GetTransformOrigin(ele, textinfos.origin);
+		_selectedtextesinfo.push_back(textinfos);
+	}
+}
 
 BENTLEY_NAMESPACE_NAME::StatusInt CreateTableTool::_OnElementModify(EditElementHandleR el)
 {
@@ -52,106 +170,45 @@ void CreateTableTool::_SetupAndPromptForNextAction()
 	mdlOutput_rscPrintf(MSG_PROMPT, NULL, STRINGLISTID_Prompts, msgId);
 }
 
-bool CreateTableTool::_FilterAgendaEntries()
-{
-
-	bool changed = false;
-	ElementAgendaR agd = GetElementAgenda();
-	//mdlDialog_dmsgsPrint(WPrintfString(L"Before Filter: The agenda number is %d", agd.GetCount()).c_str());
-
-	ElementAgenda::iterator itr = agd.begin();
-	while (itr!= agd.end())
-	{
-		int eleType = itr->GetElementType();
-		if (eleType == TEXT_ELM || eleType == TEXT_NODE_ELM)
-		{
-			++itr;
-		}
-		else
-		{
-			itr = agd.erase(itr);
-			changed = true;
-		}
-	}
-	return changed;	
-}
+//bool CreateTableTool::_FilterAgendaEntries()
+//{
+//
+//	bool changed = false;
+//	ElementAgendaR agd = GetElementAgenda();
+//	//mdlDialog_dmsgsPrint(WPrintfString(L"Before Filter: The agenda number is %d", agd.GetCount()).c_str());
+//
+//	ElementAgenda::iterator itr = agd.begin();
+//	while (itr!= agd.end())
+//	{
+//		int eleType = itr->GetElementType();
+//		if (eleType == TEXT_ELM || eleType == TEXT_NODE_ELM)
+//		{
+//			++itr;
+//		}
+//		else
+//		{
+//			itr = agd.erase(itr);
+//			changed = true;
+//		}
+//	}
+//	return changed;	
+//}
 
 bool CreateTableTool::_OnModifyComplete(DgnButtonEventCR ev)
 {
-	if (_ExtractTextInfo() == SUCCESS)
+
+	BeFileName _outexclfile;
+	if (FALSE == mdlDialog_fileCreate(_outexclfile, 0, 0, L"OutTable", L"*.xlsx,*.*", nullptr, L"输入要导出的文件名"))
 	{
-		tabelInfo.sort([](const TextInfo& left, const TextInfo& right) {return left.origin.x < right.origin.x;});
-		tabelInfo.sort([](const TextInfo& left, const TextInfo& right) {return left.origin.y > right.origin.y; });
-		BeFileName outcsvFile;
-		if (FALSE == mdlDialog_fileCreate(outcsvFile, 0, 0, L"OutTable", L"*.csv,*.*", nullptr, L"输入要导出的文件名"))
-		{
-			//mdlDialog_dmsgsPrint(outcsvFile.c_str());
-			OutputTable(outcsvFile);
-			mdlOutput_messageCenter(OutputMessagePriority::Info, WPrintfString(L"%s 创建成功!", outcsvFile), WPrintfString(L"%s 创建成功!", outcsvFile), OutputMessageAlert::None);
-		}
+		//mdlDialog_dmsgsPrint(outcsvFile.c_str());
+		_OutputTable(_outexclfile.c_str());
+		mdlOutput_messageCenter(OutputMessagePriority::Info, WPrintfString(L"%s 创建成功!", _outexclfile), WPrintfString(L"%s 创建成功!", _outexclfile), OutputMessageAlert::None);
 	}
+
+
 	return __super::_OnModifyComplete(ev);
 }
 
-BENTLEY_NAMESPACE_NAME::StatusInt CreateTableTool::_ExtractTextInfo()
-{
-	tabelInfo.clear();
-	ElementAgendaR agd = GetElementAgenda();
-	for (auto ele : agd)
-	{
-		ITextQueryCP textQueryHandler = ele.GetITextQuery();
-		if (textQueryHandler == nullptr) continue;
-		T_ITextPartIdPtrVector textPartIds;
-		textQueryHandler->GetTextPartIds(ele, *ITextQueryOptions::CreateDefault(), textPartIds);
-		//Create TextInfo
-		TextInfo textinfos;
-		for each (auto textPartId in textPartIds)
-		{
-			TextBlockPtr textBlock = textQueryHandler->GetTextPart(ele, *textPartId);
-			textinfos.m_textcontent += textBlock->ToString();
-		}
-		DisplayHandlerP  textDisplayHandler = ele.GetDisplayHandler();
-		if (textDisplayHandler == nullptr) continue;
-		textDisplayHandler->GetTransformOrigin(ele, textinfos.origin);
-		tabelInfo.push_back(textinfos);
-	}
-	if (tabelInfo.empty()) return ERROR; else return SUCCESS;	
-}
-
-void CreateTableTool::OutputTable(BeFileName outFileName)
-{
-	std::wofstream oFile(outFileName,std::ios::out);
-	oFile.imbue((std::locale)"chs");
-	double forwardy = 0;
-	double uorPerMM = ACTIVEMODEL->GetModelInfoCP()->GetUorPerMeter() / 1000;
-	for (auto itr = tabelInfo.begin(); itr != tabelInfo.end(); ++itr)
-	{
-		if (itr == tabelInfo.begin())
-		{
-			forwardy = itr->origin.y;
-			oFile << itr->m_textcontent.c_str() << ",";
-		}
-		else
-		{
-			if (std::abs(forwardy - itr->origin.y) >= g_tableytolerance.tolerance * uorPerMM)
-				oFile << std::endl;
-			oFile << itr->m_textcontent.c_str() << ",";
-			forwardy = itr->origin.y;
-		}
-	}
-	//int count = 0;
-	//for each (auto info in tabelInfo)
-	//{
-	//	oFile << info.m_textcontent.c_str() << ",";
-	//	count++;
-	//	if (count == 12)
-	//	{
-	//		oFile << std::endl;
-	//		count = 0;
-	//	}
-	//}
-	oFile.close();
-}
 
 void CreateTableTool::InstallNewInstance(int toolId)
 {
@@ -159,7 +216,17 @@ void CreateTableTool::InstallNewInstance(int toolId)
 	tool->InstallTool();
 }
 
+public ref class CreateTableActionDelegate
+{
+public:
+	static void DoAction()
+	{
+		CreateTableTool::InstallNewInstance(CMDNAME_CreateTableTool);
+	}
+};
+
 void CreateTable(WCharCP unparsed)
 {
-	CreateTableTool::InstallNewInstance(CMDNAME_CreateTableTool);
+	System::Action^ _doaction = gcnew System::Action(&CreateTableActionDelegate::DoAction);
+	PDIWT_Encrypt::Entrance::VerifyActivationState(_doaction);
 }
--- PDIWT_Gadget/PDIWT_Gadget.h
+++ PDIWT_Gadget/PDIWT_Gadget.h
@@ -10,10 +10,17 @@
 #include <DgnView\DgnTool.h>
 #include <DgnView\AccuDraw.h>
 #include <DgnView\AccuSnap.h>
+#include <Bentley\bstdset.h>
 
 #include <list>
+#include <set>
 #include <fstream>
 #include <cmath>
+#include <algorithm>
+
+#include <gcroot.h>
+#include <vcclr.h>
+#include <msclr\marshal.h>
 
 #include "PDIWT_Gadgetcmd.h"
 #include "PDIWT_Gadgetids.h"
@@ -34,3 +41,45 @@ Private void CreateTable(WCharCP);
 /* Hook Function														*/
 /************************************************************************/
 void dgnLevel_optionBtnHook(DialogItemMessageP);
+
+//#pragma comment(lib,"animcore.lib")
+//#pragma comment(lib,"BaseGeoCoord.lib")
+//#pragma comment(lib,"Bentley.GeoCoord.lib")
+//#pragma comment(lib,"Bentley.imodelconnection.lib")
+//#pragma comment(lib,"Bentley.imodelmessage.lib")
+//#pragma comment(lib,"Bentley.lib")
+//#pragma comment(lib,"Bentley.MicroStation.MaterialControl.lib")
+//#pragma comment(lib,"Bentley.PrintDefinitions.lib")
+//#pragma comment(lib,"Bentley.PrintFoundation.lib")
+//#pragma comment(lib,"BentleyAllocator.lib")
+//#pragma comment(lib,"BentleyGeom.lib")
+//#pragma comment(lib,"BentleyGeomSerialization.lib")
+//#pragma comment(lib,"BeXml.lib")
+//#pragma comment(lib,"Constraint2dCore.lib")
+//#pragma comment(lib,"Constraint2dElement.lib")
+//#pragma comment(lib,"dbpwise.lib")
+//#pragma comment(lib,"DgnGeoCoord.lib")
+//#pragma comment(lib,"DgnPlatform.lib")
+//#pragma comment(lib,"DgnView.lib")
+//#pragma comment(lib,"ECObjects.lib")
+//#pragma comment(lib,"ImageLib.lib")
+//#pragma comment(lib,"InsolationLib.lib")
+//#pragma comment(lib,"mdlbltin.lib")
+//#pragma comment(lib,"mdlxmltools.lib")
+//#pragma comment(lib,"mspsolid.lib")
+//#pragma comment(lib,"mstntoolsubs.lib")
+//#pragma comment(lib,"nativewindow.lib")
+//#pragma comment(lib,"nativewinmfc.lib")
+//#pragma comment(lib,"PointCloud.lib")
+//#pragma comment(lib,"PSolidAcisInterop.lib")
+//#pragma comment(lib,"PSolidCore.lib")
+//#pragma comment(lib,"Publish.lib")
+//#pragma comment(lib,"RasterCore.lib")
+//#pragma comment(lib,"rasterlib.lib")
+//#pragma comment(lib,"RmgrTools.lib")
+//#pragma comment(lib,"SmartFeature.lib")
+//#pragma comment(lib,"splindlm.lib")
+//#pragma comment(lib,"TerrainModelCore.lib")
+//#pragma comment(lib,"TerrainModelElementHandler.lib")
+//#pragma comment(lib,"videolib.lib")
+//#pragma comment(lib,"VisEdgesLib.lib")
